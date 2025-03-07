d1f504274feec273262f09a1862b70c1ace2946f
Abir Banerjee <abirbanerjee793@gmail.com>
2025-03-07 12:56:18 +0530

    set shdcn correctly

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,12 @@
+import {Button} from '@/components/ui/button'
+
 export default function Home() {
   return (
+    <>
     <h1 className="text-3xl text-pink-700 underline underline-offset-8 font-semibold" >
       Hello world!
     </h1>
+    <Button className='mt-[100px] cursor-pointer ml-3'>Click here</Button>
+    </>
   )
 }
